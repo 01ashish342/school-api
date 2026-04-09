210fe3b9394151e18557940d39b077c24267f202
ASHISH <ashish1510yadav@gmail.com>
2026-04-09 17:20:30 +0530

    update db.js

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
-const mysql= require('mysql2');
+const mysql = require('mysql2');
+const url = require('url');
 
-const db= mysql.createConnection(process.env.MYSQL_URL);
+const dbUrl = new url.URL(process.env.MYSQL_URL);
 
-db.connect((err) => {
-
-    if(err){
-        console.log("DB Connection Failed:", err);
-    }
-    else{
-        console.log("DB Connected Successfully");
-
-    }
-
-
+const db = mysql.createConnection({
+  host: dbUrl.hostname,
+  port: dbUrl.port,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.slice(1)
 });
 
-module.exports=db;
+db.connect((err) => {
+  if(err){
+    console.log("DB Connection Failed:", err);
+  }
+  else{
+    console.log("DB Connected Successfully");
+  }
+});
+
+module.exports = db;
